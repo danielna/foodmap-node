567587b3aa16da02a48456fa221e817a1980c6f9
Daniel Na <daniel.na@gmail.com>
2013-06-17 16:15:51 -0400

    Map infobox dependent on the load of google maps

--- site/assets/js/foodmap.init.js
+++ site/assets/js/foodmap.init.js
@@ -4,8 +4,8 @@ require.config({
         'underscore': 'lib/underscore.min',
         'backbone': 'lib/backbone.min',
         'jquery': 'lib/jquery-1.10.1.min',
-        'infobox': 'lib/infobox.packed',
         'async': 'lib/async',
+        'infobox': 'lib/infobox.packed',
         'text': 'lib/text'
     }, // set up custom paths to libraries, or paths to RequireJS plugins
     shim: {
@@ -15,6 +15,9 @@ require.config({
         'backbone': {
             deps: ['underscore', 'jquery'],
             exports: 'Backbone'
+        },
+        'infobox': {
+            deps: ['gmaps']
         }
     } // used for setting up all Shims (see below for more detail)
 });
@@ -24,7 +27,6 @@ require.config({
 define('gmaps', ['async!http://maps.googleapis.com/maps/api/js?key=AIzaSyDiA6472v_0VZUQjmVvXQ4peWbFZsUcpVc&sensor=false'], function() {
     return window.google.maps;
 });
-
 
 require(["jquery", "underscore", "backbone", "router", "gmaps"],
     function($, _, Backbone, Router) {
