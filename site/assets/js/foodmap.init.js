6ae3999dcf9a0751c56f4ad866b614d16a185381
Daniel Na <daniel.na@gmail.com>
2013-06-17 13:05:09 -0400

    Asynchronously load googlemaps using requirejs plugin "async"

--- site/assets/js/foodmap.init.js
+++ site/assets/js/foodmap.init.js
@@ -1,12 +1,11 @@
-
-
 require.config({
     baseUrl: "/assets/js", // generally the same directory as the script used in a data-main attribute for the top level script
     paths: {
         'underscore': 'lib/underscore.min',
         'backbone': 'lib/backbone.min',
         'jquery': 'lib/jquery-1.10.1.min',
-        'infobox': 'lib/infobox.packed'
+        'infobox': 'lib/infobox.packed',
+        'async': 'lib/async'
     }, // set up custom paths to libraries, or paths to RequireJS plugins
     shim: {
         'underscore': {
@@ -20,15 +19,19 @@ require.config({
 });
 
 
-require( ["jquery", "underscore", "backbone", "views/main/Main" ],
-    function($, _, Backbone, Main) {
+// convert Google Maps into an AMD module
+define('gmaps', ['async!http://maps.googleapis.com/maps/api/js?key=AIzaSyDiA6472v_0VZUQjmVvXQ4peWbFZsUcpVc&sensor=false'], function() {
+    return window.google.maps;
+});
 
-        var foodmap = foodmap || {};
 
-        $(function() {
+require(["jquery", "underscore", "backbone", "views/main/Main", "gmaps"], function($, _, Backbone, Main) {
 
-            new Main();
+    var foodmap = foodmap || {};
 
-        });
-    }
-);
+    $(function() {
+
+        new Main();
+
+    });
+});
