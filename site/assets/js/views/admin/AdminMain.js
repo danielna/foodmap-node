62762cd2092ee8f612cabb9778b4ffe0580c8e47
Daniel Na <daniel.na@gmail.com>
2013-06-17 11:28:09 -0400

    Clean up trailing white spaces and general spacing

--- site/assets/js/views/admin/AdminMain.js
+++ site/assets/js/views/admin/AdminMain.js
@@ -1,83 +1,85 @@
-
 define(["jquery", "backbone", "collections/MapItemList", "views/admin/AdminListView", "models/MapItem"],
-    
-    function($, Backbone, MapItemList, AdminListView, MapItem) {
 
-        var foodmap = foodmap || {};
+function($, Backbone, MapItemList, AdminListView, MapItem) {
 
-        foodmap.AdminMain = Backbone.View.extend({
+    var foodmap = foodmap || {};
 
-            el: "body",
+    foodmap.AdminMain = Backbone.View.extend({
 
-            events: {
-                "click #submit": "addListing",
-                "click .filter": "filterListHandler"
-            },
-            
-            initialize: function() {
-                this.$body = this.$el;
-                this.$form = this.$el.find("#add-listing");
-                this.$count = this.$body.find(".count");
+        el: "body",
 
-                this.collection = new MapItemList();
-                this.adminListView = new AdminListView({ collection: this.collection }),
+        events: {
+            "click #submit": "addListing",
+            "click .filter": "filterListHandler"
+        },
 
-                this.collection.fetch({reset: true});
+        initialize: function() {
+            this.$body = this.$el;
+            this.$form = this.$el.find("#add-listing");
+            this.$count = this.$body.find(".count");
 
-                this.listenTo( this.collection, 'add', this.resetView );
-                this.listenTo( this.adminListView, 'editListing', this.addListing );
-                this.listenTo( this.adminListView, 'deleteListing', this.deleteListing );
-            },
+            this.collection = new MapItemList();
+            this.adminListView = new AdminListView({
+                collection: this.collection
+            }),
 
-            addListing: function(e) {
-                e.preventDefault();
+            this.collection.fetch({
+                reset: true
+            });
 
-                var formData = {};
-                
-                _.each(this.$form.serializeArray(), function(data) {
-                    formData[data.name] = data.value;
-                });
+            this.listenTo(this.collection, 'add', this.resetView);
+            this.listenTo(this.adminListView, 'editListing', this.addListing);
+            this.listenTo(this.adminListView, 'deleteListing', this.deleteListing);
+        },
 
-                var ethnicity = [],
-                    tags = [],    
-                    req_e = formData.ethnicity.split(","),
-                    req_t = formData.tags.split(",");
+        addListing: function(e) {
+            e.preventDefault();
 
-                for (var ety in req_e) {
-                    ethnicity.push((req_e[ety]).trim());
-                }
+            var formData = {};
 
-                for (var t in req_t) {
-                    tags.push((req_t[t]).trim());
-                }
+            _.each(this.$form.serializeArray(), function(data) {
+                formData[data.name] = data.value;
+            });
 
-                formData.ethnicity = ethnicity;
-                formData.tags = tags;
-                
-                this.collection.create( formData );
-            },
+            var ethnicity = [],
+                tags = [],
+                req_e = formData.ethnicity.split(","),
+                req_t = formData.tags.split(",");
 
-            resetView: function() {
-                this.$form[0].reset();
-                this.$count.text(this.collection.length);
-            },
-
-            // Parse the class of the link to determine how to sort the view
-            // Todo: I don't like how coupled the markup and functionality are here.
-            filterListHandler: function(ev) {
-                ev.preventDefault();
-
-                var $link = $(ev.currentTarget),
-                    className = $link.attr("class").replace("filter ", "");
-                className = (className.indexOf("-desc") < 0) ? className + "-desc" : className.substring(0, className.length - 5);
-                
-                $link.attr("class", "filter " + className);
-                this.adminListView.renderFiltered(className);           
+            for (var ety in req_e) {
+                ethnicity.push((req_e[ety]).trim());
             }
 
-        });
+            for (var t in req_t) {
+                tags.push((req_t[t]).trim());
+            }
+
+            formData.ethnicity = ethnicity;
+            formData.tags = tags;
+
+            this.collection.create(formData);
+        },
+
+        resetView: function() {
+            this.$form[0].reset();
+            this.$count.text(this.collection.length);
+        },
+
+        // Parse the class of the link to determine how to sort the view
+        // Todo: I don't like how coupled the markup and functionality are here.
+        filterListHandler: function(ev) {
+            ev.preventDefault();
+
+            var $link = $(ev.currentTarget),
+                className = $link.attr("class").replace("filter ", "");
+            className = (className.indexOf("-desc") < 0) ? className + "-desc" : className.substring(0, className.length - 5);
+
+            $link.attr("class", "filter " + className);
+            this.adminListView.renderFiltered(className);
+        }
+
+    });
 
     return foodmap.AdminMain;
 
-    }
-);
+});
