7c35a986c77e711c09ce9dab0f74514b50a06a0d
Daniel Na <daniel.na@gmail.com>
2013-06-27 12:10:13 -0400

    Set up local auth with passport.  IT WORKS.  SHINE ON YOU CRAZY DIAMOND

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 var application_root = __dirname,
     express = require( 'express' ), //Web framework
     path = require( 'path' ), //Utilities for dealing with file paths
-    mongoose = require( 'mongoose' ); //MongoDB integration
+    mongoose = require( 'mongoose' ), //MongoDB integration
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
+
 
 //Create server
 var app = express(),
@@ -11,32 +14,60 @@ var app = express(),
 
 // Configure server
 app.configure( function() {
+    //Where to serve static content
+    app.use(express.static( path.join( application_root, 'site') ));
+    app.use(express.cookieParser());
     //parses request body and populates request.body
-    app.use( express.bodyParser() );
-
+    app.use(express.bodyParser());
+    app.use(express.session({ secret: 'eat maps' }));
+    app.use(passport.initialize());
+    app.use(passport.session());
     //checks request.body for HTTP method overrides
     app.use( express.methodOverride() );
-
     //perform route lookup based on url and HTTP method
     app.use( app.router );
-
-    //Where to serve static content
-    app.use( express.static( path.join( application_root, 'site') ) );
-
-    app.use(express.cookieParser());
-    app.use(express.bodyParser());
-    app.use(express.session({ secret: 'keyboard cat' }));
-    // app.use(passport.initialize());
-    // app.use(passport.session());
-
     //Show all errors in development
     app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-// Routes
-app.get( '/api', function( request, response ) {
-    response.send( 'Library API is running' );
+
+
+
+// Schema(s)
+var ListingSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: String,
+    ethnicity: [String],
+    tags: [String],
+    coordinates: { lat: String, lng: String},
+    created: { type: Date, default: Date.now },
+    modified: { type: Date, default: Date.now },
+    map_id: {type: Schema.Types.ObjectId, ref: 'Map'}
 });
+
+var UserSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+UserSchema.methods.validPassword = function( pwd ) {
+    return ( this.password === pwd );
+};
+
+var MapSchema = new mongoose.Schema({
+    name: String,
+    created: { type: Date, default: Date.now },
+    modified: { type: Date, default: Date.now },
+    user_id: {type: Schema.Types.ObjectId, ref: 'User'},
+    welcome_message: String
+});
+
+// Models
+var Listing = mongoose.model( 'Listing', ListingSchema );
+var User = mongoose.model( 'User', UserSchema );
+var Map = mongoose.model( 'Map', MapSchema );
+
 
 
 
@@ -51,201 +82,9 @@ db.once('open', function callback (db) {
     // I think it's only important to have this in the callback if I'm running commands in this file
     console.log("Mongoose connection open");
 
-    // Schema(s)
-    var ListingSchema = new mongoose.Schema({
-        name: String,
-        description: String,
-        price: String,
-        ethnicity: [String],
-        tags: [String],
-        coordinates: { lat: String, lng: String},
-        created: { type: Date, default: Date.now },
-        modified: { type: Date, default: Date.now },
-        map_id: {type: Schema.Types.ObjectId, ref: 'Map'}
-    });
-
-    var UserSchema = new mongoose.Schema({
-        email: String,
-        password: String
-    });
-
-    var MapSchema = new mongoose.Schema({
-        name: String,
-        created: { type: Date, default: Date.now },
-        modified: { type: Date, default: Date.now },
-        user_id: {type: Schema.Types.ObjectId, ref: 'User'},
-        welcome_message: String
-    });
-
-    // Models
-    var Listing = mongoose.model( 'Listing', ListingSchema );
-    var User = mongoose.model( 'User', UserSchema );
-    var Map = mongoose.model( 'Map', MapSchema );
-
-
-
-    //// LISTINGS
-    // Get all listings
-    app.get( '/api/listings', function( request, response ) {
-        return Listing.find( function( err, listings ) {
-            if( !err ) {
-                return response.send( listings );
-            } else {
-                return console.log( err );
-            }
-        });
-    });
-    //Get listing details
-    app.get( '/api/listings/:id', function( request, response ) {
-        return Listing.findById( request.params.id , function( err, listing ) {
-            if( !err ) {
-                return response.send( listing );
-            } else {
-                return console.log( err );
-            }
-        });
-    });
-    // Insert a new listing
-    app.post( '/api/listings', function( request, response ) {
-        var date = new Date();
-
-        var listing = new Listing({
-            name: request.body.name,
-            description: request.body.description,
-            price: request.body.price,
-            ethnicity: request.body.ethnicity,
-            tags: request.body.tags,
-            coordinates: {
-                lat: request.body.lat,
-                lng: request.body.lng
-            },
-            created: date,
-            modified: date,
-            map_id: request.body.map_id
-        });
-
-        listing.save( function( err ) {
-            if( !err ) {
-                console.log( 'Created listing: ' + request.body.name );
-                response.send(listing);
-            } else {
-                console.log( "POST Error: ", err );
-                response.send( err );
-            }
-        });
-        return response.send( listing );
-    });
-    // Update a listing
-    app.put( '/api/listings/:id', function( request, response ) {
-        console.log( 'Updating listing ' + request.body.name );
-        return Listing.findById( request.params.id, function( err, listing ) {
-            listing.name = request.body.name,
-            listing.description = request.body.description,
-            listing.price = request.body.price,
-            listing.ethnicity = request.body.ethnicity,
-            listing.tags = request.body.tags,
-            listing.coordinates.lat = request.body.lat,
-            listing.coordinates.lng = request.body.lng,
-            listing.modified = new Date();
-
-            return listing.save( function( err ) {
-                if( !err ) {
-                    console.log( 'Updated listing: ' + request.body.name );
-                    response.send(listing);
-                } else {
-                    console.log( "PUT Error: ", err );
-                    response.send(err);
-                }
-            });
-        });
-    });
-    // Delete a listing
-    app.delete( '/api/listings/:id', function( request, response ) {
-        console.log( 'Deleting listing with id: ' + request.params.id );
-        return Listing.findById( request.params.id, function( err, listing ) {
-            return listing.remove( function( err ) {
-                if( !err ) {
-                    console.log( 'Listing removed: ' + request.body.name );
-                    response.send( listing );
-                } else {
-                    console.log( err );
-                }
-            });
-        });
-    });
-
-
-
-    //// USERS
-    // Get all users
-    app.get( '/api/users', function( request, response ) {
-        return User.find( function( err, res ) {
-            if( !err ) {
-                return response.send( res );
-            } else {
-                return console.log( err );
-            }
-        });
-    });
-    // Get a single user by id
-    app.get( '/api/users/:id', function( request, response ) {
-        return User.findById( request.params.id, function( err, res ) {
-            if( !err ) {
-                return response.send( res );
-            } else {
-                return console.log( err );
-            }
-        });
-    });
-    // Get maps for a user by user_id
-    app.get( '/api/users/:id/maps', function( request, response ) {
-        return Map.find( { user_id: request.params.id }, function( err, res ) {
-            if( !err ) {
-                return response.send( res );
-            } else {
-                return console.log( err );
-            }
-        });
-    });
-
-
-
-    //// MAPS
-    // Get all maps
-    app.get( '/api/maps', function( request, response ) {
-        return Map.find( function( err, res ) {
-            if( !err ) {
-                return response.send( res );
-            } else {
-                return console.log( err );
-            }
-        });
-    });
-    // Get a single map by id
-    app.get( '/api/maps/:id', function( request, response ) {
-        return Map.findById( request.params.id, function( err, res ) {
-            if( !err ) {
-                return response.send( res );
-            } else {
-                return console.log( err );
-            }
-        });
-    });
-    // Get listings for a map by map_id
-    app.get( '/api/maps/:id/listings', function( request, response ) {
-        return Listing.find({ map_id: request.params.id }, function( err, res ) {
-            if( !err ) {
-                return response.send( res );
-            } else {
-                return console.log( err );
-            }
-        });
-    });
-
-
-// *****************
-// One-off Commands
-// *****************
+    // *****************
+    // One-off Commands
+    // *****************
 
     User.remove().exec();
     Map.remove().exec();
@@ -299,21 +138,231 @@ db.once('open', function callback (db) {
         if(error) { console.log(error); }
     });
 
-    User.find(function (err, res) {
-      if (err) {
-        console.log("error");
-      } // TODO handle err
-      console.log(res);
-    });
+    // User.find(function (err, res) {
+    //   if (err) {
+    //     console.log("error");
+    //   } // TODO handle err
+    //   console.log(res);
+    // });
 
-    Map.find(function (err, res) {
-      if (err) {
-        console.log("error");
-      } // TODO handle err
-      console.log(res);
-    });
+    // Map.find(function (err, res) {
+    //   if (err) {
+    //     console.log("error");
+    //   } // TODO handle err
+    //   console.log(res);
+    // });
 
 });
+
+
+
+
+
+
+
+// Routes
+app.get( '/api', function( request, response ) {
+    response.send( 'Library API is running' );
+});
+
+
+// Passport / Auth
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findOne(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'email'
+  },
+  function(username, password, done) {
+    User.findOne({ email: username }, function (err, user) {
+      // if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect email.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/#/home/');
+  });
+
+//// LISTINGS
+// Get all listings
+app.get( '/api/listings', function( request, response ) {
+    return Listing.find( function( err, listings ) {
+        if( !err ) {
+            return response.send( listings );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+//Get listing details
+app.get( '/api/listings/:id', function( request, response ) {
+    return Listing.findById( request.params.id , function( err, listing ) {
+        if( !err ) {
+            return response.send( listing );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+// Insert a new listing
+app.post( '/api/listings', function( request, response ) {
+    var date = new Date();
+
+    var listing = new Listing({
+        name: request.body.name,
+        description: request.body.description,
+        price: request.body.price,
+        ethnicity: request.body.ethnicity,
+        tags: request.body.tags,
+        coordinates: {
+            lat: request.body.lat,
+            lng: request.body.lng
+        },
+        created: date,
+        modified: date,
+        map_id: request.body.map_id
+    });
+
+    listing.save( function( err ) {
+        if( !err ) {
+            console.log( 'Created listing: ' + request.body.name );
+            response.send(listing);
+        } else {
+            console.log( "POST Error: ", err );
+            response.send( err );
+        }
+    });
+    return response.send( listing );
+});
+// Update a listing
+app.put( '/api/listings/:id', function( request, response ) {
+    console.log( 'Updating listing ' + request.body.name );
+    return Listing.findById( request.params.id, function( err, listing ) {
+        listing.name = request.body.name,
+        listing.description = request.body.description,
+        listing.price = request.body.price,
+        listing.ethnicity = request.body.ethnicity,
+        listing.tags = request.body.tags,
+        listing.coordinates.lat = request.body.lat,
+        listing.coordinates.lng = request.body.lng,
+        listing.modified = new Date();
+
+        return listing.save( function( err ) {
+            if( !err ) {
+                console.log( 'Updated listing: ' + request.body.name );
+                response.send(listing);
+            } else {
+                console.log( "PUT Error: ", err );
+                response.send(err);
+            }
+        });
+    });
+});
+// Delete a listing
+app.delete( '/api/listings/:id', function( request, response ) {
+    console.log( 'Deleting listing with id: ' + request.params.id );
+    return Listing.findById( request.params.id, function( err, listing ) {
+        return listing.remove( function( err ) {
+            if( !err ) {
+                console.log( 'Listing removed: ' + request.body.name );
+                response.send( listing );
+            } else {
+                console.log( err );
+            }
+        });
+    });
+});
+
+
+
+//// USERS
+// Get all users
+app.get( '/api/users', function( request, response ) {
+    return User.find( function( err, res ) {
+        if( !err ) {
+            return response.send( res );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+// Get a single user by id
+app.get( '/api/users/:id', function( request, response ) {
+    return User.findById( request.params.id, function( err, res ) {
+        if( !err ) {
+            return response.send( res );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+// Get maps for a user by user_id
+app.get( '/api/users/:id/maps', function( request, response ) {
+    return Map.find( { user_id: request.params.id }, function( err, res ) {
+        if( !err ) {
+            return response.send( res );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+
+
+
+//// MAPS
+// Get all maps
+app.get( '/api/maps', function( request, response ) {
+    return Map.find( function( err, res ) {
+        if( !err ) {
+            return response.send( res );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+// Get a single map by id
+app.get( '/api/maps/:id', function( request, response ) {
+    return Map.findById( request.params.id, function( err, res ) {
+        if( !err ) {
+            return response.send( res );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+// Get listings for a map by map_id
+app.get( '/api/maps/:id/listings', function( request, response ) {
+    return Listing.find({ map_id: request.params.id }, function( err, res ) {
+        if( !err ) {
+            return response.send( res );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+
+
+
 
 
 //Start server
@@ -323,7 +372,7 @@ app.listen( port, function() {
 });
 
 // db.maps.find().pretty()
-// db.listings.update({ tags: "BestOf" }, { $set: { map_id: ObjectId("51cb51f87268d88734000001") } }, { multi: true })
-// db.listings.update({ tags: "Pizza" }, { $set: { map_id: ObjectId("51cb51f87268d88734000002") } }, { multi: true })
-// db.listings.update({ tags: "Burgers" }, { $set: { map_id: ObjectId("51cb51f87268d88734000003") } }, { multi: true })
+// db.listings.update({ tags: "BestOf" }, { $set: { map_id: ObjectId("51cc4fc56f955c3a12000001") } }, { multi: true })
+// db.listings.update({ tags: "Pizza" }, { $set: { map_id: ObjectId("51cc4fc56f955c3a12000002") } }, { multi: true })
+// db.listings.update({ tags: "Burgers" }, { $set: { map_id: ObjectId("51cc4fc56f955c3a12000003") } }, { multi: true })
 // db.listings.find( { $or: [ {tags:"BestOf"}, {tags:"Burgers"}, {tags:"Pizza"} ] }).pretty()
